0a30306eafe92687bd33cb2efb52407b74e2a959
Matieu Lagarde <matieu.lagarde@usm.edu>
2018-08-22 14:00:00 -0500

    Pretend to allow sorting responses

--- src/components/ResponseList/Header.js
+++ src/components/ResponseList/Header.js
@@ -1,19 +1,41 @@
 // Library imports
 import React from 'react';
 
-const Header = () => (
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Date Submitted</th>
-      <th>Visit Date</th>
-      <th>User ID</th>
-      <th>Clinic ID</th>
-      <th>Patient ID</th>
-      <th>Patient DOB</th>
-      <th>Assessments</th>
-    </tr>
-  </thead>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      responseID: 1,
+    };
+  }
+
+  handleClick = (event) => {
+    const name = event.target.attributes.name.value;
+    this.setState({
+      [name]: ((this.state[name] || 0) + 1) % 3,
+    });
+  }
+
+  helper = (target, name) => (
+    <th name={target} onClick={this.handleClick}>
+      {name} {[' ', '▾', '▴'][this.state[target]]}
+    </th>
+  )
+
+  render = () => (
+    <thead>
+      <tr>
+        {this.helper('responseID', 'ID')}
+        {this.helper('dateSubmitted', 'Date Submitted')}
+        {this.helper('visitDate', 'Visit Date')}
+        {this.helper('userID', 'User ID')}
+        {this.helper('clinicID', 'Clinic ID')}
+        {this.helper('patientID', 'Patient ID')}
+        {this.helper('patientDOB', 'Patient DOB')}
+        <th>Assessments</th>
+      </tr>
+    </thead>
+  )
+}
 
 export default Header;
