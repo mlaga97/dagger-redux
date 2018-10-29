41d6de09e428723d6fe8fde6bace74a121bd83ec
Mike Graham <mike.grhm@gmail.com>
2018-10-29 18:26:57 -0500

    Add Last Week Row to User Statistics table

--- src/components/UserStatistics.js
+++ src/components/UserStatistics.js
@@ -30,14 +30,14 @@ class ResponseList extends React.Component {
     }
 
     let {
-      today,
-      weekNumber,
-      weekStartDate,
-      weekEndDate,
-      appointmentCount,
-      warmHandoffCount,
-      hchCount,
-      recordCount
+      thisWeek_today, lastWeek_today,
+      thisWeek_weekNumber, lastWeek_weekNumber,
+      thisWeek_weekStartDate, lastWeek_weekStartDate,
+      thisWeek_weekEndDate, lastWeek_weekEndDate,
+      thisWeek_appointmentCount, lastWeek_appointmentCount,
+      thisWeek_warmHandoffCount, lastWeek_warmHandoffCount,
+      thisWeek_hchCount, lastWeek_hchCount,
+      thisWeek_recordCount, lastWeek_recordCount
     } = this.props.statistics.user;
 
     return (
@@ -63,12 +63,20 @@ class ResponseList extends React.Component {
                   </thead>
                   <tbody>
                     <tr>
-                      <td>This Week (week {weekNumber})</td>
-                      <td>{weekStartDate} - {weekEndDate}</td>
-                      <td>{appointmentCount}</td>
-                      <td>{warmHandoffCount}</td>
-                      <td>{hchCount}</td>
-                      <td>{recordCount}</td>
+                      <td>This Week (week {thisWeek_weekNumber})</td>
+                      <td>{thisWeek_weekStartDate} - {thisWeek_weekEndDate}</td>
+                      <td>{thisWeek_appointmentCount}</td>
+                      <td>{thisWeek_warmHandoffCount}</td>
+                      <td>{thisWeek_hchCount}</td>
+                      <td>{thisWeek_recordCount}</td>
+                    </tr>
+                    <tr>
+                      <td>Last Week (week {lastWeek_weekNumber})</td>
+                      <td>{lastWeek_weekStartDate} - {lastWeek_weekEndDate}</td>
+                      <td>{lastWeek_appointmentCount}</td>
+                      <td>{lastWeek_warmHandoffCount}</td>
+                      <td>{lastWeek_hchCount}</td>
+                      <td>{lastWeek_recordCount}</td>
                     </tr>
                   </tbody>
                 </Table>
