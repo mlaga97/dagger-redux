91100c9028544b3e9271517840375a3f04fa9c9e
Mike Graham <mike.grhm@gmail.com>
2018-11-12 15:49:10 -0600

    Set min, max for AssessmentDate

--- src/components/AssessmentPage/Metadata/AssessmentDate.js
+++ src/components/AssessmentPage/Metadata/AssessmentDate.js
@@ -4,6 +4,19 @@ import React from 'react';
 // Components
 import FocusableInput from '../../FocusableInput';
 
+// TODO: Use server Date
+let getMaxYesterday = function() {
+   let d = new Date();
+   d.setDate(d.getDate() - 1);
+   return d.toLocaleDateString('en-CA');
+};
+
+let getMinOneYearAgo = function() {
+   let y = new Date();
+   y.setFullYear(y.getFullYear() - 1);
+   return y.toLocaleDateString('en-CA');
+};
+
 const AssessmentDate = ({metadata, onChange}) => {
   if (metadata && metadata.assessmentDateToday === 'true') {
     return null;
@@ -17,6 +30,8 @@ const AssessmentDate = ({metadata, onChange}) => {
       name='assessmentDate'
       label='Assessment Date'
       onChange={onChange}
+      max={getMaxYesterday()}
+      min={getMinOneYearAgo()}
     />
   );
 }
