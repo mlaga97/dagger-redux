602cf13b8d3b631d1669bb4db2a6a9aa09f07bfb
Mike Graham <mike.grhm@gmail.com>
2018-11-12 16:19:13 -0600

    Set min, max for PatientDOB

--- src/components/AssessmentPage/Metadata/PatientDOB.js
+++ src/components/AssessmentPage/Metadata/PatientDOB.js
@@ -4,6 +4,13 @@ import React from 'react';
 // Components
 import FocusableInput from '../../FocusableInput';
 
+// Minimum allowed patient age is 1 year old as of today
+let getMaxOneYearAgo = function() {
+   let y = new Date();
+   y.setFullYear(y.getFullYear() - 1);
+   return y.toLocaleDateString('en-CA');
+};
+
 // TODO: Make controlled
 const PatientDOB = ({onChange}) => (
   <FocusableInput
@@ -12,6 +19,8 @@ const PatientDOB = ({onChange}) => (
     label='Patient DOB'
     required='required'
     onChange={onChange}
+    max={getMaxOneYearAgo()}
+    min='1900-01-01'
   />
 );
 
