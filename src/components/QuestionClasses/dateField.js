afe530951310c0e717842a539f7b0e744bc4477b
Mike Graham <mike.grhm@gmail.com>
2018-11-12 15:52:31 -0600

    Set min, max for QuestionClasses dateField
    All dates currently collected except DOB and assessment date conform to range today back one year.

--- src/components/QuestionClasses/dateField.js
+++ src/components/QuestionClasses/dateField.js
@@ -11,6 +11,19 @@ let formatDate = function(input) {
   return input.replace(pattern, '$2/$3/$1');
 };
 
+// Currently this min, max should apply to all Dagger dates except DOB, AssessmentDate which use FocusableInput
+let getMaxToday = function() {
+   let d = new Date();
+   return d.toLocaleDateString('en-CA');
+};
+
+let getMinOneYearAgoPlusOneDay = function() {
+   let y = new Date();
+   y.setDate(y.getDate() + 1);            // Tomorrow
+   y.setFullYear(y.getFullYear() - 1);    // One year back from Tomorrow (excludes today's date last year)
+   return y.toLocaleDateString('en-CA');
+};
+
 // TODO: This component is only dubiously controlled, and that should be fixed.
 class Renderer extends React.Component {
   handleChange = (event) => {
@@ -59,6 +72,8 @@ class Renderer extends React.Component {
           defaultValue={value}
           onBlur={this.handleChange}
           required={(required) ? 'required' : null}
+          max={getMaxToday()}
+          min={getMinOneYearAgoPlusOneDay()}
         />
       </FormGroup>
     );
