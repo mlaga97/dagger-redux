0fe7754a044f75229062a66beab25ce63c79d2b4
Matieu Lagarde <matieu.lagarde@usm.edu>
2018-11-15 11:22:28 -0600

    Add response/list sagas

--- src/sagas/response/index.js
+++ src/sagas/response/index.js
@@ -1,5 +1,5 @@
 // Library imports
-import { takeLatest } from 'redux-saga/effects';
+import { takeEvery, takeLatest } from 'redux-saga/effects';
 
 // Actions
 import actions from '../../actions';
@@ -7,11 +7,13 @@ import actions from '../../actions';
 // Sagas
 import all from './all';
 import get from './get';
+import list from './list';
 import post from './post';
 
 // Export saga
 export default function* () {
   yield takeLatest(actions.response.all.requested, all);
   yield takeLatest(actions.response.get.requested, get);
+  yield takeEvery(actions.response.list.requested, list);
   yield takeLatest(actions.response.post.requested, post);
 }
